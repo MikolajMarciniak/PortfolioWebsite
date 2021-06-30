808468d1767ac7367b2d6b4146e971268ef60be6
Astaethiel <marciniak.miko@gmail.com>
2021-06-29 19:44:40 -0500

    Added empty hamburger menu container

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,6 +14,11 @@ function Navbar() {
           <a href="#about">about</a>
           <a href="#projects">projects</a>
           <a href="#contact">contact</a>
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
