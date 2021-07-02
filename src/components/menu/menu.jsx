abd56006059c29eb8cba35a9d81f24a0f1630d6b
Astaethiel <marciniak.miko@gmail.com>
2021-07-02 09:01:15 +0200

    Replaced rendering of menu with map function

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,23 +1,23 @@
 import "./menu.scss";
 
-export default function Menu() {
+const sectionLinks = [
+  { title: "about me", link: "#about" },
+  { title: "projects", link: "#projects" },
+  { title: "contact me", link: "#contact" },
+];
+
+export default function Menu({ menuOpen, setMenuOpen }) {
   return (
-    <div className="ham-menu">
-      <ul>
-        <li>
-          <a href="#about">about me</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href="#projects">projects</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href="#contact">contact me</a>
-        </li>
-      </ul>
+    <div className={"ham-menu " + (menuOpen && "active")}>
+      {sectionLinks.map((section) => (
+        <ul>
+          <li>
+            <a href={section.link} onClick={() => setMenuOpen()}>
+              {section.title}
+            </a>
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
