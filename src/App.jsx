913a784d8bf088e05e4d0e3979817ff62848131f
Astaethiel <marciniak.miko@gmail.com>
2021-07-06 17:36:00 +0200

    Undid merging errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import "./app.scss";
 import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import Menu from "./components/menu/menu";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const isMobile = useMediaQuery({ query: `(max-width: 1000px)` });
   return (
     <div className="app">
-      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Navbar
+        menuOpen={menuOpen}
+        setMenuOpen={setMenuOpen}
+        isMobile={isMobile}
+      />
+      {isMobile && <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Menu>}
       <div className="sections">
         <Landing />
         <About />
