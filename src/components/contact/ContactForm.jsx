55f875e7835428b8cdc5850b75f761da90072f0a
Astaethiel <marciniak.miko@gmail.com>
2021-07-08 23:32:39 +0200

    Created basic form infrastructure

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
-export default function ContactForm() {
-  return <div></div>;
-}
+const ContactForm = ({}) => {
+  return (
+    <div>
+      <h2>Do you have a question?</h2>
+      <p>Leave me a message. I will happily answer all questions.</p>
+      <form className="contact-form">
+        <div className="inputs">
+          <input
+            type="text"
+            name="email"
+            id="email-input"
+            placeholder="Email address"
+          />
+          <div className="invalid-feedback"></div>
+        </div>
+        <div className="inputs">
+          <textarea
+            type="text"
+            name="desc"
+            autoComplete="off"
+            id="description-input"
+            rows="3"
+            placeholder="Your message"
+          />
+          <div className="invalid-feedback"></div>
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
