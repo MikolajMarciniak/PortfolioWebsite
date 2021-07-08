c01d489d82710b4f3ac4a236ab7502b9b44cbbba
Astaethiel <marciniak.miko@gmail.com>
2021-07-08 23:26:47 +0200

    imported ContactForm into Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,10 @@
 import "./contact.scss";
+import ContactForm from "./ContactForm";
 
 export default function Contact() {
   return (
     <div className="contact" id="contact">
-      contact
+      <ContactForm></ContactForm>
     </div>
   );
 }
